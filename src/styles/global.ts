25a3d16b53c3e647dd4dad33150523ba89292658
Arthur Sena <senaarth@gmail.com>
2022-08-04 02:05:36 -0300

    feat: title component and next image css reset

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,7 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Open Sans', sans-serif;
+    font-family: "Open Sans", sans-serif;
   }
 
   html, body, #__next {
@@ -18,7 +18,7 @@ export default createGlobalStyle`
   }
 
   .playfair {
-    font-family: 'Playfair Display', serif;
+    font-family: "Playfair Display", serif;
   }
 
   a {
@@ -26,6 +26,63 @@ export default createGlobalStyle`
     
     &:hover {
       filter: brightness(0.8);
+    }
+  }
+
+  .title {
+    color: ${({ theme }) => theme.dark};
+    font-family: "Playfair Display", serif;
+    font-size: 1.5rem;
+    font-weight: 500;
+    line-height: 125%;
+
+    display: flex;
+    position: relative;
+
+    &::after {
+      content: "";
+
+      position: absolute;
+      left: 0;
+      bottom: -0.625rem;
+
+      height: 0.5rem;
+      width: 90%;
+      max-width: 12ch;
+
+      background-color: ${({ theme }) => theme.light};
+    }
+
+    &.translate-highlight {
+      padding-left: 0.75ch; 
+      
+      &::after {
+        left: -0.25ch;
+      }
+    }
+
+    @media (min-width: 768px) {
+      font-size: 2rem;
+    }
+  }
+
+  .image-wrapper {
+    position: relative;
+
+    > div {
+      position: relative !important;
+      inset: unset !important;
+      display: flex !important;
+      width: 100% !important;
+
+      img {
+        width: 100%;
+        height: unset !important;
+        object-fit: cover;
+
+        position: relative !important;
+        height: 100% !important;
+      }
     }
   }
 `;
