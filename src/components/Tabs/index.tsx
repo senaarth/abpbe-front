2f9a4a8766d0cc8a28f3f1e8bc65ee9088544004
Lucas Andrade <lafdesouza2002@outlook.com>
2022-08-18 09:50:47 -0300

    feat: Create tabs component

---
+++ src/components/Tabs/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Tab, Tabs as TabGroup, TabList } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { Content } from './styles';
+
+type TabType = {
+    label: string,
+    key: string,
+    disabled: boolean,
+}
+
+interface TabsProps {
+    tabs: TabType[];
+    // eslint-disable-next-line no-unused-vars
+    onChange: (e) => void;
+}
+
+export const Tabs = ({ tabs, onChange }: TabsProps) => {
+  return (
+    <Content>
+        <TabGroup>
+            <TabList onSelect={onChange}>
+                {tabs.map(tab => {
+                    return <Tab key={tab.key} disabled={tab.disabled}>{tab.label?.toUpperCase()}</Tab>
+                })}
+            </TabList>
+        </TabGroup>
+    </Content>
+  )
+}
