46553290c69680662b7c04c1ec6153dd812834bf
Arthur Sena <senaarth@gmail.com>
2022-09-01 15:44:41 -0300

    feat: meta data, favicon, title and description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -55,6 +55,36 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
+
+          <link
+            rel="icon"
+            type="image/svg"
+            sizes="any"
+            href="./images/icon.svg"
+          />
+
+          <meta name="msapplication-TileImage" content="./images/icon.svg" />
+
+          <meta
+            property="og:site_name"
+            content="Associação Brasileira de Psicologia Baseada em Evidências"
+          />
+          <meta
+            property="og:title"
+            content="Associação Brasileira de Psicologia Baseada em Evidências"
+          />
+          <meta
+            property="og:description"
+            content="Aqui você encontrará conteúdos sobre os mais diversos temas relevantes para a Psicologia Baseada em Evidências, desde discussões clássicas até as últimas novidades da ciência."
+          />
+
+          <meta property="og:image" content="./images/icon.svg" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:image:type" content="image/svg" />
+
+          <meta property="og:image:width" content="200" />
+          <meta property="og:image:height" content="200" />
         </Head>
         <body>
           <Main />
