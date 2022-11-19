ecac67256f2f891518184aac24a4fe78af961089
senaarth <senaarth@gmail.com>
2022-11-19 11:47:36 -0300

    feat: divulgacao page api integration

--- src/pages/divulgacao.tsx
+++ src/pages/divulgacao.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Head from "next/head";
+import req from "superagent";
 
 import { PageBanner } from "../components/PageBanner";
 import { Footer } from "../components/Footer";
 import { PageCall } from "../components/PageCall";
 
+import { PageData } from "../@types/pageData";
+
 import { ContentContainer, Page } from "../styles/Divulgacao";
 
-export default function Divulgacao(): JSX.Element {
+interface DivulgacaoProps {
+  pageData: PageData;
+}
+
+export default function Divulgacao({ pageData }: DivulgacaoProps): JSX.Element {
   return (
     <Page>
       <Head>
@@ -15,35 +22,66 @@ export default function Divulgacao(): JSX.Element {
       </Head>
       <PageBanner
         img="/images/bg_glasses.png"
-        title="Divulgação científica"
-        subtitle="Em breve você encontrará textos sobre os mais diversos temas relevantes para a Psicologia Baseada em Evidências, desde discussões clássicas até as últimas novidades da ciência."
+        title={pageData?.bannerTitle || "Divulgação científica"}
+        subtitle={
+          pageData?.bannerSubtitle ||
+          "Em breve você encontrará textos sobre os mais diversos temas relevantes para a Psicologia Baseada em Evidências, desde discussões clássicas até as últimas novidades da ciência."
+        }
         whiteSubtitle
-        tag="Novidades"
+        tag={pageData?.tag || "Novidades"}
       />
       <ContentContainer>
         <h1 className="title translate-highlight">Divulgação científica</h1>
         <p>
-          Sabemos que nem todo artigo já publicado - mesmo que em periódicos de
-          alto impacto - é realmente de qualidade. Ao mesmo tempo, para
-          colocarmos em prática uma Psicologia Baseada em Evidências, é
-          importante que tenhamos como referência artigos capazes de apresentar
-          boas provas sobre o tema que investigam e a tese que defendem.
-          <br />
-          <br />A discriminação do nível de qualidade de um trabalho científico
-          é uma tarefa complexa, que requer adaptações e procedimentos
-          diferentes para cada tipo de delineamento, tema de investigação e
-          objetivo de pesquisa. De modo a facilitar o acesso a materiais de
-          referência e da mais alta qualidade, foi desenvolvido o projeto da
-          Galeria de Artigos da ABPBE. A nossa Galeria é um espaço destinado à
-          exposição exclusiva de artigos de grande excelência relevantes para a
-          Psicologia e áreas afins. Em breve publicaremos os artigos revisados!
+          {pageData?.pageDescription ? (
+            pageData.pageDescription
+          ) : (
+            <>
+              Sabemos que nem todo artigo já publicado - mesmo que em periódicos
+              de alto impacto - é realmente de qualidade. Ao mesmo tempo, para
+              colocarmos em prática uma Psicologia Baseada em Evidências, é
+              importante que tenhamos como referência artigos capazes de
+              apresentar boas provas sobre o tema que investigam e a tese que
+              defendem.
+              <br />
+              <br />A discriminação do nível de qualidade de um trabalho
+              científico é uma tarefa complexa, que requer adaptações e
+              procedimentos diferentes para cada tipo de delineamento, tema de
+              investigação e objetivo de pesquisa. De modo a facilitar o acesso
+              a materiais de referência e da mais alta qualidade, foi
+              desenvolvido o projeto da Galeria de Artigos da ABPBE. A nossa
+              Galeria é um espaço destinado à exposição exclusiva de artigos de
+              grande excelência relevantes para a Psicologia e áreas afins. Em
+              breve publicaremos os artigos revisados!
+            </>
+          )}
         </p>
       </ContentContainer>
-      <PageCall
-        boldTitle="Em breve"
-        regularTitle=" postaremos os textos de divulgação científica!"
-      />
+      {pageData?.pageCall && (
+        <PageCall
+          playfairFont
+          title={pageData?.pageCall || null}
+          boldTitle={!pageData?.pageCall ? null : "Em breve"}
+          regularTitle={
+            !pageData?.pageCall
+              ? null
+              : " postaremos os textos de divulgação científica!"
+          }
+        />
+      )}
       <Footer />
     </Page>
   );
+}
+
+export async function getServerSideProps() {
+  const { body: pageData } = await req.get(
+    `${process.env.NEXT_PUBLIC_API}/pages/eventos`
+  );
+
+  return {
+    props: {
+      pageData,
+    },
+  };
 }
