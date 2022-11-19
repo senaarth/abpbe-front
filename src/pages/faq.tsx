b4213ea7e14d41d00ea601b41247b7193758a8cd
senaarth <senaarth@gmail.com>
2022-11-19 12:33:18 -0300

    feat: faq page api integration

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Head from "next/head";
+import req from "superagent";
 
 import { AccordionComponent } from "../components/Accordion";
 import { PageBanner } from "../components/PageBanner";
 import { Footer } from "../components/Footer";
 
+import { PageData } from "../@types/pageData";
+
 import { Page, ContentContainer } from "../styles/Faq";
-import { api } from "../services/api";
 
 type Question = {
   summary: string;
@@ -15,9 +17,10 @@ type Question = {
 
 interface FaqProps {
   questions: Question[];
+  pageData: PageData;
 }
 
-export default function Faq({ questions }: FaqProps): JSX.Element {
+export default function Faq({ questions, pageData }: FaqProps): JSX.Element {
   return (
     <Page>
       <Head>
@@ -25,16 +28,19 @@ export default function Faq({ questions }: FaqProps): JSX.Element {
       </Head>
       <PageBanner
         img=""
-        title="Perguntas frequentes"
-        subtitle="Aqui você encontrará textos sobre os mais diversos temas relevantes para a Psicologia Baseada em Evidências, desde discussões clássicas até as últimas novidades da ciência."
+        title={pageData?.bannerTitle || "Perguntas frequentes"}
+        subtitle={
+          pageData?.bannerSubtitle ||
+          "Aqui você encontrará textos sobre os mais diversos temas relevantes para a Psicologia Baseada em Evidências, desde discussões clássicas até as últimas novidades da ciência."
+        }
       />
       <ContentContainer>
-        <h1 className="title translate-highlight">Perguntas Frequentes</h1>
+        <h1 className="title translate-highlight">
+          {pageData?.pageTitle || "Perguntas Frequentes"}
+        </h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-          lacus ex, sit amet blandit leo lobortis eget.
+          {pageData?.pageDescription ||
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."}
         </p>
         <AccordionComponent questions={questions} />
       </ContentContainer>
@@ -44,9 +50,14 @@ export default function Faq({ questions }: FaqProps): JSX.Element {
 }
 
 export async function getServerSideProps() {
-  const { data } = await api.get("/questions");
+  const { body: pageData } = await req.get(
+    `${process.env.NEXT_PUBLIC_API}/pages/faq`
+  );
+  const { body: faqData } = await req.get(
+    `${process.env.NEXT_PUBLIC_API}/questions`
+  );
 
-  const questions = data?.reduce((acc, curr) => {
+  const questions = faqData?.reduce((acc, curr) => {
     return [
       ...acc,
       {
@@ -61,6 +72,7 @@ export async function getServerSideProps() {
   return {
     props: {
       questions,
+      pageData,
     },
   };
 }
